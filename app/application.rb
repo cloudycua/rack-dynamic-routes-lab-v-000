ef0805fc82822462aa0267a8daef86a845206795
Christine Cua <cloudycua@gmail.com>
2018-06-25 13:59:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,9 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-
+    if req.path=="/testing"
+      resp.write "Route not found"
+      resp.status = 404
   end
 
 end
